6de3673b1c6a366412e6f5c49c3c56b6ff7c55a6
Salvatore Santamaria <sam.santamaria@gmail.com>
2021-01-03 07:35:23 -0800

    Child to Parent Communication, make console.log clearer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,6 @@ export class AppComponent {
   //child to parent communication 4: 
   // takes in data from the child component via the app.component.html file
   onTerm(term: string) {
-    console.log('I am the app and here is the term: from onTerm function')
+    console.log('From onTerm function. I am the app and here is the Term: ' + term)
   }
 }
